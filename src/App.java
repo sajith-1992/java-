ef569ae11793aecc2931c0a5cb5177d7be5be74f
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-02-03 17:18:36 -0500

    java sum

--- src/App.java
+++ src/App.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
          Scanner sc = new Scanner(System.in);
-        int  a = sc.nextInt();
-        int b=sc.nextInt();
+        System.out.println("entera");
+         int  a = sc.nextInt();
+        System.out.println("enter b");
+         int b=sc.nextInt();
         int sum=a+b;
         System.out.println(sum);
         sc.close();
