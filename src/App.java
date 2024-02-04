e874247041da1b7c042299895c5b2f37cc2f8b70
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-02-03 23:51:34 -0500

    static variable

--- src/App.java
+++ src/App.java
@@ -1,24 +1,10 @@
-import java.util.Scanner;
+
 
 public class App {
+    static int a=10;
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-    System.out.println("enter the number");
-
-    sampl s1=new sampl();
-    sampl h3=new sampl();
-
-    s1.a=1000;
-    s1.b=5000;
-    h3.a=3000;
-    h3.b=9000;
-   
-    
-   
-    s1.display();
   
-    
+    System.out.println(a);
 
-            sc.close();
-    }}
-    
+    }
+}
