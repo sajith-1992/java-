1cb11616c207e5f43e521f2eed0e717d2aeaa555
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-02-03 23:54:53 -0500

    static function

--- src/App.java
+++ src/App.java
@@ -2,9 +2,13 @@
 
 public class App {
     static int a=10;
+    static void hello(){
+        System.out.println("hai");
+  }
     public static void main(String[] args) throws Exception {
   
     System.out.println(a);
+      hello();
 
     }
-}
+  }
